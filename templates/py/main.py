46c3143837e41fa6625a52b0152ac56660934c9f
Rik van der Kooij <rik.van.der.kooij@c3.ai>
2023-12-06 16:38:14 +0100

    Add python template

---
+++ templates/py/main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    part1 = 0
+    part2 = 0
+
+    for line in sys.stdin:
+        pass
+
+    print(part1)
+    print(part2)
+
+
+if __name__ == "__main__":
+    main()
