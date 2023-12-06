0ab7204f28caf9efbd1db8b1647958bbcbfc1ffd
Rik van der Kooij <rik.van.der.kooij@c3.ai>
2023-12-07 00:27:22 +0100

    Add configurable input to run script

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 year=${AOC_YEAR:-$(date +%Y)}
 day=${AOC_DAY:-$(date +%d)}
 lang=${AOC_LANG:-py}
+input=${AOC_INPUT:-input}
 
 if [ $lang = "py" ]; then
-    python3 ./$year/$day/py/main.py < ./$year/$day/input
+    python3 ./$year/$day/py/main.py < ./$year/$day/$input
 else
     echo "unrecognized language '$lang'"
 fi
