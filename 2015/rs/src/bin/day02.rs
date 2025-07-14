1ae932b892cb7566c4f69b0645d469040aed58e5
Rik van der Kooij <rikvanderkooij@gmail.com>
2025-07-14 13:37:53 +0200

    Add helper method for readability

--- 2015/rs/src/bin/day02.rs
+++ 2015/rs/src/bin/day02.rs
@@ -1,25 +1,28 @@
-use itertools::Itertools;
 use std::io;
+
+use itertools::Itertools;
+
+fn parse_dimensions(s: &str) -> [i32; 3] {
+    s.split('x')
+        .map(|t| t.parse().expect("Failed to parse numbers"))
+        .sorted()
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap()
+}
 
 fn main() {
     let mut part1 = 0;
     let mut part2 = 0;
 
-    io::stdin()
-        .lines()
-        .filter_map(Result::ok)
-        .map(|line| {
-            line.split('x')
-                .map(|s| s.parse().expect("Failed to parse numbers"))
-                .sorted()
-                .collect::<Vec<_>>()
-                .try_into()
-        })
-        .filter_map(Result::<[i32; 3], Vec<i32>>::ok)
-        .for_each(|[l, w, h]| {
-            part1 += 3 * l * w + 2 * w * h + 2 * l * h;
-            part2 += 2 * l + 2 * w + l * w * h;
-        });
+    let lines = io::stdin().lines().map(Result::unwrap);
+
+    for line in lines {
+        let [l, w, h] = parse_dimensions(&line);
+
+        part1 += 3 * l * w + 2 * w * h + 2 * l * h;
+        part2 += 2 * l + 2 * w + l * w * h;
+    }
 
     println!("{}", part1);
     println!("{}", part2);
